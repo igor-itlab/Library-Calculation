409f7fb32f305a80fdd75a871b1fcdfa1b6c4e39
Oleksandra <sasatamcan@itlab-studio.com>
2020-08-07 12:08:24 +0300

    add requires in composer json

--- Entity/ProviderInterface.php
+++ Entity/ProviderInterface.php
@@ -4,10 +4,14 @@
 namespace Calculation\Entity;
 
 
+
+use Doctrine\Common\Collections\Collection;
+
 interface ProviderInterface
 {
     /**
      * @return string
      */
     public function getName(): string;
+    public function getProviderCommission(): Collection;
 }
